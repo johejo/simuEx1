c5e55482c421d42d70bbd8ffb5772ed108d4f483
mitsuo <mitsuo_h@outlook.com>
2017-06-29 17:34:58 +0900

    fix

--- ave.py
+++ ave.py
@@ -1,5 +1,7 @@
 import sys
 s = 0
+n = 0
 for l in sys.stdin.readlines():
     s += float(l)
-print(s)
+    n += 1
+print(s / n)
--- result.sh
+++ result.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-rm -rf cwnd*.dat thp*.dat *.tr *.nam win parameters queue-mon
-
-for i in `seq 1 20`
+for j in `seq 1 10`
 do
-    #statements
-    ns exp1.tcl $i >> thpave$i.txt
-    cat thpave.txt | python sum.py >> thpsum$i.txt
-    # seq 1 $i | paste - thpave$i.dat >> thpave_p$i.dat
+    for i in `seq 1 20`
+    do
+        #statements
+        ns exp1.tcl $i > thpave$i.txt
+        cat thpave.txt | python sum.py >> thpsum$i.txt
+        cat thpave.txt | python ave.py >> thpaveave$i.txt
+        # seq 1 $i | paste - thpave$i.dat >> thpave_p$i.dat
+    done
 done
